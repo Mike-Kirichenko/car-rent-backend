62a74d2e6ac69223b2a341ceaf902330b5a3ccd8
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-10-28 17:41:53 +0400

    dates diff fix

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -7,11 +7,17 @@ export class CarService {
 
   async checkAvaliability(dto: { id: number; dateFrom: Date; dateTo: Date }) {
     const { id, dateFrom, dateTo } = dto;
+
+    const dateFromFinalDate = new Date(dateFrom);
+    const dateFromDiff = dateFromFinalDate.getDate() + 3;
+    dateFromFinalDate.setDate(dateFromDiff);
+
     const dateToFinalDate = new Date(dateTo);
-    const deff = dateToFinalDate.getDate() + 3;
-    dateToFinalDate.setDate(deff);
+    const dateToDiff = dateToFinalDate.getDate() + 3;
+    dateToFinalDate.setDate(dateToDiff);
+
     const query = `SELECT * FROM car WHERE id NOT IN (SELECT "carId" FROM rent_list 
-    WHERE 'rent_list.dateFrom' >= '${dateFrom}' 
+    WHERE 'rent_list.dateFrom' >= '${dateFromFinalDate}' 
     AND 'rent_list.dateTo' <= '${dateToFinalDate}')`;
     const avaliableCars = await this.queryBuilder.runQuery(query);
     if (id) {
