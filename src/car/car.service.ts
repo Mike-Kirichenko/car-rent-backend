c457c5cbdeefe3b62a00c1803530536df7f76a26
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-10-28 16:35:23 +0400

    func fix

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -4,13 +4,20 @@ import { QueryBuilder } from 'src/common/queryBuilder';
 @Injectable()
 export class CarService {
   constructor(private queryBuilder: QueryBuilder) {}
-  async checkAvaliability(dto: { id: number; dateFrom: Date; dateTo: Date }) {
+
+  checkAvaliability = async (dto: {
+    id: number;
+    dateFrom: Date;
+    dateTo: Date;
+  }) => {
     const { id, dateFrom, dateTo } = dto;
     const dateToFinalDate = new Date(dateTo);
     const deff = dateToFinalDate.getDate() + 3;
     dateToFinalDate.setDate(deff);
+
     const query = `SELECT "carId" FROM rent_list WHERE 'rent_list.dateFrom' >= '${dateFrom}' AND 'rent_list.dateTo' <= '${dateToFinalDate}'`;
     const rentedCars = await this.queryBuilder.runQuery(query);
+
     if (rentedCars) {
       if (id) {
         const rentedIndex = rentedCars.findIndex(
@@ -25,5 +32,5 @@ export class CarService {
         return await this.queryBuilder.runQuery(query);
       }
     }
-  }
+  };
 }
