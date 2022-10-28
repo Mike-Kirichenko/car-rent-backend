9bf80bbb779c53b1731e562875fd93d57cdf1501
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-10-28 17:31:32 +0400

    query refactored

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -5,32 +5,21 @@ import { QueryBuilder } from 'src/common/queryBuilder';
 export class CarService {
   constructor(private queryBuilder: QueryBuilder) {}
 
-  checkAvaliability = async (dto: {
-    id: number;
-    dateFrom: Date;
-    dateTo: Date;
-  }) => {
+  async checkAvaliability(dto: { id: number; dateFrom: Date; dateTo: Date }) {
     const { id, dateFrom, dateTo } = dto;
     const dateToFinalDate = new Date(dateTo);
     const deff = dateToFinalDate.getDate() + 3;
     dateToFinalDate.setDate(deff);
-
-    const query = `SELECT "carId" FROM rent_list WHERE 'rent_list.dateFrom' >= '${dateFrom}' AND 'rent_list.dateTo' <= '${dateToFinalDate}'`;
-    const rentedCars = await this.queryBuilder.runQuery(query);
-
-    if (rentedCars) {
-      if (id) {
-        const rentedIndex = rentedCars.findIndex(
-          (rentedCar: { carId: number }) => rentedCar.carId == id,
-        );
-        return { carId: id, avaliable: rentedIndex > -1 };
-      } else {
-        const rentedCarIds = rentedCars
-          .map((rentedCar: { carId: number }) => rentedCar.carId)
-          .join(',');
-        const query = `SELECT * FROM car WHERE id NOT IN (${rentedCarIds})`;
-        return await this.queryBuilder.runQuery(query);
-      }
+    const query = `SELECT * FROM car WHERE id NOT IN (SELECT "carId" FROM rent_list 
+    WHERE 'rent_list.dateFrom' >= '${dateFrom}' 
+    AND 'rent_list.dateTo' <= '${dateToFinalDate}')`;
+    const avaliableCars = await this.queryBuilder.runQuery(query);
+    if (id) {
+      const avaliableCarIndex = avaliableCars.findIndex(
+        (car: { id: number; name: string; LP: string }) => car.id === id,
+      );
+      return { carId: id, avaliable: avaliableCarIndex > -1 };
     }
-  };
+    return avaliableCars;
+  }
 }
--- src/car/dto/getCarInfo.dto.ts
+++ src/car/dto/getCarInfo.dto.ts
@@ -2,6 +2,6 @@ import { Matches } from 'class-validator';
 import { IdDto } from 'src/common/dto/id.dto';
 
 export class GetCarsInfoDto extends IdDto {
-  @Matches('^((0[1-9])|(1[0-2]))-([0-9]{4})$')
+  @Matches('^([0-9]{4})-((0[1-9])|(1[0-2]))$')
   month: string;
 }
