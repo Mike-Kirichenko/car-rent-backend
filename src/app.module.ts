2ee73504b8e6aaa0a934acbd17e92279618d6c33
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-11-17 16:29:58 +0400

    manual migration migrates to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
 import { CarModule } from './car/car.module';
 import { RentModule } from './rent/rent.module';
 
 @Module({
   imports: [CarModule, RentModule, ConfigModule.forRoot({ isGlobal: true })],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  conn: any;
+  constructor(private config: ConfigService) {
+    this.conn = new Pool({
+      user: this.config.get('DB_USER'),
+      host: this.config.get('DB_HOST'),
+      database: this.config.get('DB_NAME'),
+      password: this.config.get('DB_PASSWORD'),
+      port: this.config.get('DB_PORT'),
+    });
+  }
+  async onModuleInit() {
+    const carQuery = `CREATE TABLE IF NOT EXISTS "car" (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (50) NOT NULL,
+      "LP" VARCHAR (10) UNIQUE NOT NULL
+    )`;
+    const rentListQuery = `CREATE TABLE IF NOT EXISTS "rent_list" (
+      id SERIAL PRIMARY KEY,
+      "carId" SMALLINT NOT NULL,
+      "dateFrom" timestamp without time zone NOT NULL,
+      "dateTo" timestamp without time zone NOT NULL,
+      "totalPrice" double precision
+    )`;
+    try {
+      console.log('migration');
+      await this.conn.query(carQuery);
+      await this.conn.query(rentListQuery);
+    } catch ({ message: msg }) {
+      console.log({ msg });
+    }
+  }
+}
--- src/common/classes/queryBuilder.ts
+++ src/common/classes/queryBuilder.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
 
 @Injectable()
-export class QueryBuilder implements OnModuleInit {
+export class QueryBuilder {
   conn: any;
   constructor(private config: ConfigService) {
     this.conn = new Pool({
@@ -13,27 +13,6 @@ export class QueryBuilder implements OnModuleInit {
       password: this.config.get('DB_PASSWORD'),
       port: this.config.get('DB_PORT'),
     });
-  }
-
-  async onModuleInit(): Promise<void> {
-    const carQuery = `CREATE TABLE IF NOT EXISTS "car" (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR (50) NOT NULL,
-      "LP" VARCHAR (10) UNIQUE NOT NULL
-    )`;
-    const rentListQuery = `CREATE TABLE IF NOT EXISTS "rent_list" (
-      id SERIAL PRIMARY KEY,
-      "carId" SMALLINT NOT NULL,
-      "dateFrom" timestamp without time zone NOT NULL,
-      "dateTo" timestamp without time zone NOT NULL,
-      "totalPrice" double precision
-    )`;
-    try {
-      await this.conn.query(carQuery);
-      await this.conn.query(rentListQuery);
-    } catch ({ message: msg }) {
-      console.log({ msg });
-    }
   }
 
   async runQuery(query: string) {
