edd0688962044644b5d8363d93c1bb4eb207d0e0
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-12-07 14:18:06 +0400

    write by chunks to db

--- src/cars-import/cars-import.controller.ts
+++ src/cars-import/cars-import.controller.ts
@@ -1,10 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+import { CarsImportService } from './cars-import.service';
 
 @Controller()
 export class CarsImportController {
+  constructor(private carsImportService: CarsImportService) {}
+
   @EventPattern('get_chunk')
-  async getCarListChunk(data: any) {
-    return console.log(data);
+  async getCarListChunk(data: string[]) {
+    this.carsImportService.writeCarsToDbByChunks(data);
   }
 }
--- src/cars-import/cars-import.service.ts
+++ src/cars-import/cars-import.service.ts
@@ -1,4 +1,12 @@
+import { QueryBuilder } from '@classes/queryBuilder';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CarsImportService {}
+export class CarsImportService {
+  constructor(private queryBuilder: QueryBuilder) {}
+  public async writeCarsToDbByChunks(data: string[]) {
+    const [name, LP] = data;
+    const query = `INSERT INTO car ("name", "LP") VALUES ('${name}', '${LP}')`;
+    await this.queryBuilder.runQuery(query);
+  }
+}
